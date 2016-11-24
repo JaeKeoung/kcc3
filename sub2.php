3346cada8a8f2800b156f648814bc857861918a2
JaeKeoung <tkdlgks@naver.com>
2016-11-24 11:30:17 +0900

    공지사항용 config를 만들어놔서 코드들을 줄여놨다.

--- sub2.php
+++ sub2.php
@@ -1,13 +1,6 @@
 ﻿<!DOCTYPE html>
 <?php
-    try {
-    $pdo = new PDO('mysql:host=localhost; dbname=kccnotice; charset=utf8', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
-    }
-    catch(PDOException $Exception) {
-        die("접속 오류: ".$Exception->getMessage()."<br>");
-    }
+    require_once 'sub2NoticeConfig.php';
     
     $sql = 'select * from board order by idx desc';
     $stmh = $pdo->prepare($sql);
